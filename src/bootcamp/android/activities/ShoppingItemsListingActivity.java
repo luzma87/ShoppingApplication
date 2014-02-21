2a54b31e96ae7a219e0892ddb4991cedadc2c27e
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2014-02-21 13:16:28 +0530

    Abhinav/Naz: Asynchronous response and image fetching.

--- src/bootcamp/android/activities/ShoppingItemsListingActivity.java
+++ src/bootcamp/android/activities/ShoppingItemsListingActivity.java
@@ -1,12 +1,16 @@
 package bootcamp.android.activities;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.StrictMode;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidplugins.Callback;
 import bootcamp.android.R;
 import bootcamp.android.adapters.ShoppingItemsListAdapter;
 import bootcamp.android.models.Product;
@@ -15,22 +19,30 @@ import bootcamp.android.repositories.ProductRepository;
 import java.util.ArrayList;
 import static bootcamp.android.constants.Constants.*;
 
-
 public class ShoppingItemsListingActivity extends Activity {
 
+    private ProductRepository productRepository;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
         final GridView gridView = (GridView) findViewById(R.id.grid_view);
+        final ProgressDialog progressDialog = ProgressDialog.show(this, "", "Loading...", true, true);
         ProductRepository productRepository = new ProductRepository();
-        ArrayList<Product> products = productRepository.getProducts();
+        productRepository.getProducts(productsCallback(gridView, progressDialog));
+    }
 
+    private Callback<ArrayList<Product>> productsCallback(final GridView gridView, final ProgressDialog progressDialog) {
+        return new Callback<ArrayList<Product>>() {
+            @Override
+            public void execute(ArrayList<Product> products) {
+                renderProducts(gridView, products);
+                progressDialog.dismiss();
+            }
+        };
+    }
+    private void renderProducts(GridView gridView, ArrayList<Product> products) {
         gridView.setAdapter(new ShoppingItemsListAdapter(this, products));
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
